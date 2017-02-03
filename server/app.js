b6d28453e181088485a85a9ab565fc9adab6ea5e
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2017-02-04 07:55:30 +0900

    スペルミス修正

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ router.get('/hello', (req, res) => {
 });
 
 router.post('/api/face', (req, res) => {
-    socketComn.send(wdd, req.body);
+    socketComn.send(wss, req.body);
     res.end();
 });
 
